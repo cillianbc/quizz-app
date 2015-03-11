777122c681d84be39d01a21a4ea1e564b91516a4
Cillianbc <cillianbc@gmail.com>
2015-03-11 20:36:28 +1100

    Array of Questions vreated

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var questions =[
+  var Question1 = {
+    Question:"A regulation volleyball game is played to ____ points, but you must win by 2 points.",
+    Answer1:"11",
+    Answer2:"21",
+    Answer3:"15",
+    Answer4:"25"
+  }
+  var Question2 = {
+    Question:"What is it called when you step on or over the line while serving?",
+    Answer1:"Side out",
+    Answer2:"Foot fault",
+    Answer3:"Let",
+    Answer4:"Net violation"
+  }
+  var Question3 = {
+    Question:"What is a type of pass where the hands are clenched together and contact is made between the wrists and elbows?",
+    Answer1:"Set",
+    Answer2:"Serve",
+    Answer3:"Forearm Pass",
+    Answer4:"Overhead Pass"
+  }
+  var Question4 = {
+    Question:"If a player hits the ball twice consecutively, it is called what?",
+    Answer1:"Double Hit",
+    Answer2:"New Violation",
+    Answer3:"Fault",
+    Answer4:"Attack"
+  }
+]
