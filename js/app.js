d268fc203ce5a1b22601be79b144edf5131767c5
Cillianbc <cillianbc@gmail.com>
2015-03-13 23:44:38 +1100

    Cosmetic changes

--- js/app.js
+++ js/app.js
@@ -3,45 +3,46 @@ $(document).ready(function() {
   {
     question:"Question #1: A regulation volleyball game is played to ____ points, but you must win by 2 points.",
     choices : [23,21,19,25],
-    fact:"In US College volleyball the game is played to 30 points",
+    fact:"<h3>Did you Know</h3>In US College volleyball the game is played to 30 points",
     correct:3
   },
   {
     question:"Question #2: What is it called when you step on or over the line while serving?",
     choices:["Side out","Foot fault","Let","Net violation",],
-    fact:"Volleyball is one of the most ppopular sports in the world",
+    fact:"<h3>Did you Know</h3>Volleyball is one of the most popular sports in the world",
     correct:1
   },
   {
     question:"Question #3: What is a type of pass where the hands are clenched together and contact is made between the wrists and elbows?",
     choices:["Set","Serve","Forearm Pass","Overhead Pass"],
-    fact:"The setter is the most specialised position on the court",
+    fact:"<h3>Did you Know</h3>The setter is the most specialised position on the court",
     correct:2
   },
   {
     question:"Question #4 : If a player hits the ball twice consecutively, it is called what?",
     choices:["Double Hit","Attack","Fault","New Violation"],
-    fact:"The indoor court is 9m x 9m, outdoor beach courts are 8m x 8m",
+    fact:"<h3>Did you Know</h3>The indoor court is 9m x 9m, outdoor beach courts are 8m x 8m",
     correct:0
   }
 ]
-
+var newQuestion = function(){
+  return '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><ul class="answers"><li><input type="radio" name="option" class="option" value="0"><span class="answer">'+" "+questions[currentQuestion].choices[0]+'</span><br></li><li><input type="radio" name="option" class="option" value="1"><span class="answer">'+" "+questions[currentQuestion].choices[1]+'</span><br></li><li><input type="radio" name="option" class="option" value="2"><span class="answer">'+" "+questions[currentQuestion].choices[2]+'</span><br></li><li><input type="radio" name="option" class="option" value="3"><span class="answer">'+" "+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
+};
 
 var numberCorrect = 0;
   var currentQuestion = 0;
-$("#question_wrapper").on("click", "#submit", function () {
-        progress();
-        currentQuestion++;
-        nextQuestion();
-    });
+  $("#question_wrapper").on("click", "#submit", function () {
+    progress();
+    currentQuestion++;
+    nextQuestion();
+  });
 
-$("#question_wrapper").on("click", "#retry_button", function () {
-        numberCorrect = 0;
-        currentQuestion = 0;
-        $("#progress").text("0% complete");
-        var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><ul class="answers"><li><input type="radio" name="option" class="option" value="0"><span class="answer">'+" "+questions[currentQuestion].choices[0]+'</span><br></li><li><input type="radio" name="option" class="option" value="1"><span class="answer">'+" "+questions[currentQuestion].choices[1]+'</span><br></li><li><input type="radio" name="option" class="option" value="2"><span class="answer">'+" "+questions[currentQuestion].choices[2]+'</span><br></li><li><input type="radio" name="option" class="option" value="3"><span class="answer">'+" "+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
-        $("#question_wrapper").html(newQuestion);
-        $("#last__fact").html("");
+  $("#question_wrapper").on("click", "#retry_button", function () {
+    numberCorrect = 0;
+    currentQuestion = 0;
+    $("#progress").text("0% complete");
+    $("#question_wrapper").html(newQuestion(currentQuestion));
+    $('#fact_holder').hide();
     });
 
 
@@ -71,20 +72,16 @@ function nextQuestion(){
     $("#answer_holder input").remove();
     $("#answer_holder span").remove();
     $("#fact").hide();
-    var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><ul class="answers"><li><input type="radio" name="option" class="option" value="0"><span class="answer">'+" "+questions[currentQuestion].choices[0]+'</span><br></li><li><input type="radio" name="option" class="option" value="1"><span class="answer">'+" "+questions[currentQuestion].choices[1]+'</span><br></li><li><input type="radio" name="option" class="option" value="2"><span class="answer">'+" "+questions[currentQuestion].choices[2]+'</span><br></li><li><input type="radio" name="option" class="option" value="3"><span class="answer">'+" "+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
-    $("#question_wrapper").html(newQuestion);
-    var lastFact= questions[currentQuestion-1].fact;
-    $("#last_fact").html(lastFact).fadeIn();
+    $("#question_wrapper").html(newQuestion(currentQuestion));
+    $("#fact_holder").addClass("fact").html(questions[currentQuestion-1].fact);
       }
   else {
     $(".question").remove();
     $("#answer_holder input").remove();
     $("#answer_holder span").remove();
-    $("#last_fact").fadeOut();
     $("#submit").css("display", "none");
     $("#retry_button").css("display", "inline");
-    var lastFact= questions[currentQuestion-1].fact;
-    $("#last_fact").html(lastFact);
+    $("#fact_holder").addClass("fact").html(questions[currentQuestion-1].fact);
       if (numberCorrect == 1) {
         var finalScore = '<span id="final">Congratulations on finishing the quiz!  You correctly answered '+numberCorrect+' question.'
         $("#answer_holder").html(finalScore);
